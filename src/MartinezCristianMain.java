c30cdcd42eadb68a69c3c0b62837b651291dbde6
CristianMarNav <cristianmartineznavarro4@gmail.com>
2024-12-02 19:44:38 +0100

    Initial commit: Creación del proyecto y estructura básica

---
+++ src/MartinezCristianMain.java
@@ -0,0 +1,5 @@
+public class MartinezCristianMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/com/aboutme/MartinezCristianPerfil.java
@@ -0,0 +1,4 @@
+package com.aboutme;
+
+public class MartinezCristianPerfil {
+}
