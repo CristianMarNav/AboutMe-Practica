bba684748af51729a0c490fba14d53ad3fa988c9
CristianMarNav <cristianmartineznavarro4@gmail.com>
2024-12-03 20:21:16 +0100

    Implementado el menú interactivo con la historia, hobbies, comidas y datos curiosos

--- src/MartinezCristianMain.java
+++ src/MartinezCristianMain.java
@@ -1,11 +1,16 @@
 package com.aboutme;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
+/**
+ * Clase que representa el perfil del usuario.
+ * Incluye atributos como nombre, historia personal, hobbies, comidas favorita y datos curiosos.
+ */
 public class MartinezCristianMain {
-    public static void main(String[] args) {
 
-        // 1. Crear listas para los hobbies, comidas favoritas y datos curiosos
+    public static void main(String[] args) {
+        // 1. Crear listas para los hobbies, comidas favoritas y datos curiosos.
         ArrayList<String> hobbies = new ArrayList<>();
         hobbies.add("Leer libros");
         hobbies.add("Jugar videojuegos");
@@ -21,7 +26,7 @@ public class MartinezCristianMain {
         funFacts.add("Tengo miedo a las alturas");
         funFacts.add("Es mi primer año en DAM");
 
-        // Crear un objeto de MartinezCristianPerfil usando el constructor
+        // 2.Crear un objeto de MartinezCristianPerfil usando el constructor.
         MartinezCristianPerfil perfil = new MartinezCristianPerfil(
                 "Cristian Martínez",
                 "Soy un estudiante de DAM que le gusta la programación.",
@@ -29,6 +34,53 @@ public class MartinezCristianMain {
                 foods,
                 funFacts
         );
+
+        // 5. Crear el Scanner para leer las opciones.
+        Scanner input = new Scanner(System.in);
+        int opcion;
+
+        // Menú interactivo.
+        do {
+            System.out.println("\nMenú AboutMe: [" + perfil.getName() + "]");
+            System.out.println("1. Story: Mostrar la historia personal del perfil");
+            System.out.println("2. Favorites: Mostrar los hobbies y comidas favoritas");
+            System.out.println("3. Fun Facts: Mostrar un dato curioso aleatorio");
+            System.out.println("4. Salir");
+
+            System.out.println("Elige una opción");
+            opcion = input.nextInt();
+
+            // Switch para manejar las opciones.
+            switch (opcion) {
+
+                case 1:
+                    // Mostrar historia.
+                    System.out.println("\nHistoria: " + perfil.getStory());
+                    break;
+
+                case 2:
+                    // Mostrar hobbies y comidas favoritas.
+                    System.out.println("Hobbies: " + perfil.getHobbies());
+                    System.out.println("Comidas favoritas: " + perfil.getFoods());
+                    break;
+
+                case 3:
+                    // Mostrar fun fact aleatorio.
+                    System.out.println("Dato curioso: " + perfil.getRandomFunFact());
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo...");
+                    break;
+
+                default:
+                    System.out.println("Opción no válida. Por favor elige una opción entre 1 y 4.");
+                    break;
+            }
+        } while (opcion !=4);
+
+        // Cerrar el scanner
+        input.close();
 
         // 3. Probar los getters
 
